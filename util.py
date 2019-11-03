100e4d3c8ff16389c13bae2f47d76637be696526
sshleifer <sshleifer@gmail.com>
2019-11-03 10:41:23 -0800

    util update

--- util.py
+++ util.py
@@ -163,11 +163,11 @@ def load_dataset(dataset_dir, batch_size, valid_batch_size= None, test_batch_siz
     return data
 
 
-def cheaper_metric(preds, labels, null_val=np.nan):
+def cheaper_metric(preds, labels, null_val=0.):
     if np.isnan(null_val):
         mask = ~torch.isnan(labels)
     else:
-        mask = (labels!=null_val)
+        mask = (labels != null_val)
     mask = mask.float()
     mask /= torch.mean(mask)
     mask = torch.where(torch.isnan(mask), torch.zeros_like(mask), mask)
