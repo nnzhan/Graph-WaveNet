63724899957cf6e0a20016eeaabca8eba41a23e5
sshleifer <sshleifer@gmail.com>
2019-11-06 15:58:23 -0800

    Dont check in utest  side effects

--- test_gwnet.py
+++ test_gwnet.py
@@ -3,10 +3,20 @@ import test
 import unittest
 from durbango import pickle_load
 import pandas as pd
+import os
+import shutil
+
 TRAIN_ARGS = 'test_args.pkl'
 TEST_ARGS = 'test_script_args.pkl'
+SAVE_DIR = 'utest_experiment/'
 
 class TestTrain(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        if os.path.exists(SAVE_DIR):
+            shutil.rmtree(SAVE_DIR)
+        os.makedirs(SAVE_DIR)
 
     def test_1_epoch(self):
         args = pickle_load(TRAIN_ARGS)
@@ -14,6 +24,7 @@ class TestTrain(unittest.TestCase):
         args.n_iters = 1
         args.batch_size = 2
         args.n_obs = 2
+        args.save = SAVE_DIR
         main(args)
         df = pd.read_csv(f'{args.save}/metrics.csv', index_col=0)
         self.assertEqual(df.shape, (2,6))
