aeb22ca14d362983c9ae68d91182d08fad74bf15
sshleifer <sshleifer@gmail.com>
2019-12-01 21:57:03 -0800

    style model.py

--- model.py
+++ model.py
@@ -1,23 +1,22 @@
 import torch
 import torch.nn as nn
+from torch.nn import BatchNorm2d, Conv1d, Conv2d, ModuleList, Parameter
 import torch.nn.functional as F
-from torch.autograd import Variable
-import sys
 
 
 def nconv(x, A):
-    """Multiply along source node axis"""
+    """Multiply x by adjacency matrix along source node axis"""
     return torch.einsum('ncvl,vw->ncwl', (x, A)).contiguous()
 
 class GraphConvNet(nn.Module):
     def __init__(self, c_in, c_out, dropout, support_len=3, order=2):
         super().__init__()
         c_in = (order * support_len + 1) * c_in
-        self.mlp = torch.nn.Conv2d(c_in, c_out, kernel_size=(1, 1), padding=(0,0), stride=(1,1), bias=True)
+        self.final_conv = Conv2d(c_in, c_out, (1, 1), padding=(0, 0), stride=(1, 1), bias=True)
         self.dropout = dropout
         self.order = order
 
-    def forward(self, x, support):
+    def forward(self, x, support: list):
         out = [x]
         for a in support:
             x1 = nconv(x, a)
@@ -28,16 +27,17 @@ class GraphConvNet(nn.Module):
                 x1 = x2
 
         h = torch.cat(out, dim=1)
-        h = self.mlp(h)
+        h = self.final_conv(h)
         h = F.dropout(h, self.dropout, training=self.training)
         return h
 
 
 class GWNet(nn.Module):
     def __init__(self, device, num_nodes, dropout=0.3, supports=None, do_graph_conv=True,
-                 softmax_temp=1.,
-                 addaptadj=True, aptinit=None, in_dim=2, out_dim=12, residual_channels=32, dilation_channels=32,
-                 skip_channels=256, end_channels=512, kernel_size=2, blocks=4, layers=2, apt_size=10):
+                 softmax_temp=1., addaptadj=True, aptinit=None, in_dim=2, out_dim=12,
+                 residual_channels=32, dilation_channels=32,
+                 skip_channels=256, end_channels=512, kernel_size=2, blocks=4, layers=2,
+                 apt_size=10):
         super().__init__()
         self.dropout = dropout
         self.softmax_temp = softmax_temp
@@ -45,14 +45,6 @@ class GWNet(nn.Module):
         self.layers = layers
         self.do_graph_conv = do_graph_conv
         self.addaptadj = addaptadj
-
-        # Each of these will
-        self.filter_convs = nn.ModuleList()
-        self.gate_convs = nn.ModuleList()
-        self.residual_convs = nn.ModuleList()
-        self.skip_convs = nn.ModuleList()
-        self.bn = nn.ModuleList()
-        self.graph_convs = nn.ModuleList()
 
         self.start_conv = nn.Conv2d(in_channels=in_dim,
                                     out_channels=residual_channels,
@@ -63,39 +55,45 @@ class GWNet(nn.Module):
         self.supports_len = len(self.fixed_supports)
         if do_graph_conv and addaptadj:
             if aptinit is None:
-                nodevec1 = torch.randn(num_nodes, apt_size)
-                nodevec2 = torch.randn(apt_size, num_nodes)
+                nodevec1, nodevec2 = torch.randn(num_nodes, apt_size), torch.randn(apt_size, num_nodes)
             else:
-                m, p, n = torch.svd(aptinit)
-                nodevec1 = torch.mm(m[:, :apt_size], torch.diag(p[:apt_size] ** 0.5))
-                nodevec2 = torch.mm(torch.diag(p[:apt_size] ** 0.5), n[:, :apt_size].t())
+                nodevec1, nodevec2 = self.svd_init(apt_size, aptinit)
             self.supports_len += 1
-            
-            self.register_parameter('nodevec1', nn.Parameter(nodevec1.to(device), requires_grad=True))
-            self.register_parameter('nodevec2', nn.Parameter(nodevec2.to(device), requires_grad=True))
-        if do_graph_conv:
-            self.graph_convs = nn.ModuleList([GraphConvNet(dilation_channels, residual_channels, dropout, support_len=self.supports_len) for i in range(blocks*layers)])
+            self.register_parameter('nodevec1', Parameter(nodevec1.to(device), requires_grad=True))
+            self.register_parameter('nodevec2', Parameter(nodevec2.to(device), requires_grad=True))
+
+        depth = blocks * layers
+
+        # 1x1 convolution for residual and skip connections (slightly different see docstring)
+        self.residual_convs = ModuleList([Conv1d(dilation_channels, residual_channels, (1, 1)) for _ in range(depth)])
+        self.skip_convs = ModuleList([Conv1d(dilation_channels, skip_channels, (1, 1)) for _ in range(depth)])
+        self.bn = ModuleList([BatchNorm2d(residual_channels) for _ in range(depth)])
+        self.graph_convs = ModuleList([GraphConvNet(dilation_channels, residual_channels, dropout, support_len=self.supports_len)
+                                              for _ in range(depth)])
+
+        self.filter_convs = ModuleList()
+        self.gate_convs = ModuleList()
         for b in range(blocks):
             additional_scope = kernel_size - 1
             D = 1 # dilation
             for i in range(layers):
                 # dilated convolutions
-                self.filter_convs.append(nn.Conv2d(residual_channels, dilation_channels, kernel_size=(1,kernel_size), dilation=D))
-                self.gate_convs.append(nn.Conv1d(residual_channels, dilation_channels, kernel_size=(1, kernel_size), dilation=D))
-
-                # 1x1 convolution for residual and skip connections (slightly different see docstring)
-                self.residual_convs.append(nn.Conv1d(dilation_channels, residual_channels, kernel_size=(1, 1)))
-                self.skip_convs.append(nn.Conv1d(dilation_channels, skip_channels, kernel_size=(1, 1)))
-                self.bn.append(nn.BatchNorm2d(residual_channels))
+                self.filter_convs.append(Conv2d(residual_channels, dilation_channels, (1,kernel_size), dilation=D))
+                self.gate_convs.append(Conv1d(residual_channels, dilation_channels, (1, kernel_size), dilation=D))
                 D *= 2
                 receptive_field += additional_scope
                 additional_scope *= 2
-
-        self.end_conv_1 = nn.Conv2d(skip_channels, end_channels, kernel_size=(1, 1), bias=True)
-        self.end_conv_2 = nn.Conv2d(end_channels, out_dim, kernel_size=(1, 1), bias=True)
-
         self.receptive_field = receptive_field
 
+        self.end_conv_1 = Conv2d(skip_channels, end_channels, (1, 1), bias=True)
+        self.end_conv_2 = Conv2d(end_channels, out_dim, (1, 1), bias=True)
+
+    @staticmethod
+    def svd_init(apt_size, aptinit):
+        m, p, n = torch.svd(aptinit)
+        nodevec1 = torch.mm(m[:, :apt_size], torch.diag(p[:apt_size] ** 0.5))
+        nodevec2 = torch.mm(torch.diag(p[:apt_size] ** 0.5), n[:, :apt_size].t())
+        return nodevec1, nodevec2
 
     @classmethod
     def from_args(cls, args, device, supports, aptinit, **kwargs):
@@ -106,7 +104,6 @@ class GWNet(nn.Module):
                     residual_channels=args.nhid, dilation_channels=args.nhid,
                     skip_channels=args.nhid * 8, end_channels=args.nhid * 16, **kwargs)
         return model
-
 
     def forward(self, input):
         # Input shape is (bs, features, n_nodes, n_timesteps)
