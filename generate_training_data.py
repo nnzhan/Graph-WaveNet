ae06854905e543fd46a63f26a70664910a67e542
sshleifer <sshleifer@gmail.com>
2019-12-05 08:35:28 -0800

    Some cleanup

--- generate_training_data.py
+++ generate_training_data.py
@@ -27,27 +27,23 @@ def generate_graph_seq2seq_io_data(
 
     num_samples, num_nodes = df.shape
     data = np.expand_dims(df.values, axis=-1)
-    data_list = [data]
+    feature_list = [data]
     if add_time_in_day:
         time_ind = (df.index.values - df.index.values.astype("datetime64[D]")) / np.timedelta64(1, "D")
         time_in_day = np.tile(time_ind, [1, num_nodes, 1]).transpose((2, 1, 0))
-        data_list.append(time_in_day)
+        feature_list.append(time_in_day)
     if add_day_in_week:
         day_in_week = np.zeros(shape=(num_samples, num_nodes, 7))
         day_in_week[np.arange(num_samples), :, df.index.dayofweek] = 1
-        data_list.append(day_in_week)
+        feature_list.append(day_in_week)
 
-    data = np.concatenate(data_list, axis=-1)
-    # epoch_len = num_samples + min(x_offsets) - max(y_offsets)
+    data = np.concatenate(feature_list, axis=-1)
     x, y = [], []
-    # t is the index of the last observation.
     min_t = abs(min(x_offsets))
     max_t = abs(num_samples - abs(max(y_offsets)))  # Exclusive
-    for t in range(min_t, max_t):
-        x_t = data[t + x_offsets, ...]
-        y_t = data[t + y_offsets, ...]
-        x.append(x_t)
-        y.append(y_t)
+    for t in range(min_t, max_t):  # t is the index of the last observation.
+        x.append(data[t + x_offsets, ...])
+        y.append(data[t + y_offsets, ...])
     x = np.stack(x, axis=0)
     y = np.stack(y, axis=0)
     return x, y
@@ -72,28 +68,22 @@ def generate_train_val_test(args):
 
     print("x shape: ", x.shape, ", y shape: ", y.shape)
     # Write the data into npz file.
-    # num_test = 6831, using the last 6831 examples as testing.
-    # for the rest: 7/8 is used for training, and 1/8 is used for validation.
     num_samples = x.shape[0]
     num_test = round(num_samples * 0.2)
     num_train = round(num_samples * 0.7)
     num_val = num_samples - num_test - num_train
-
-    # train
     x_train, y_train = x[:num_train], y[:num_train]
-    # val
     x_val, y_val = (
         x[num_train: num_train + num_val],
         y[num_train: num_train + num_val],
     )
-    # test
     x_test, y_test = x[-num_test:], y[-num_test:]
 
     for cat in ["train", "val", "test"]:
         _x, _y = locals()["x_" + cat], locals()["y_" + cat]
         print(cat, "x: ", _x.shape, "y:", _y.shape)
         np.savez_compressed(
-            os.path.join(args.output_dir, "%s.npz" % cat),
+            os.path.join(args.output_dir, f"cat.npz"),
             x=_x,
             y=_y,
             x_offsets=x_offsets.reshape(list(x_offsets.shape) + [1]),
